ac19510d84fb88afe4f8982ae0af61a2259a66f1
Vinzenz Rosenkranz <vinzenz.rosenkranz@gmail.com>
2014-11-25 04:28:37 +0100

    small redesign of user/group selection

--- templates/start.php
+++ templates/start.php
@@ -9,9 +9,9 @@
 	<div class="new_poll">
 		<h1><?php p($l->t('Create new poll')); ?></h1>
 		<label for="text_title" class="label_h1 input_title"><?php p($l->t('Title')); ?></label>
-		<input type="text" id="text_title" name="text_title"/>
+		<input type="text" class="input_field" id="text_title" name="text_title"/>
 		<label for="text_desc" class="label_h1 input_title"><?php p($l->t('Description')); ?></label>
-		<textarea cols="50" rows="5" id="text_desc" name="text_desc"></textarea>
+		<textarea cols="50" rows="5" style="width: auto;" class="input_field" id="text_desc" name="text_desc"></textarea>
 
 		<div class="input_title"><?php p($l->t('Access')); ?></div>
 
@@ -22,50 +22,41 @@
 		<label for="public"><?php p($l->t('Public access')); ?></label>
 
 		<input type="radio" name="radio_pub" id="select" value="select">
-		<label for="select">Select...</label>
+		<label for="select"><?php p($l->t('Select')); ?></label>
 
 		<input type="hidden" name="access_group_ids" value="" />
-
-        <br>
-
+        <br/>
 		<input type="submit" id="submit_create_poll" value="<?php p($l->t('Next')); ?>" />
 	</div>
 
-
-	<div id="dialog-overlay"></div>
+	<!--<div id="dialog-overlay"></div>-->
 	<div id="dialog-box">
 		<div id="dialog-message"></div>
-
 		<table id="table_access">
 			<tr>
 				<td>
 					<table id="table_groups">
 						<tr>
-							<th>Groups</th>
+							<th><?php p($l->t('Groups')); ?></th>
 						</tr>
-							<?php
-
-							$groups = OC_Group::getUserGroups(OC_User::getUser());
-							foreach($groups as $gid) {
-								echo '<tr><td class="cl_group_item">' . $gid . '</td></tr>';
-							}
-
-							?>
-							?>
+							<?php $groups = OC_Group::getUserGroups(OC_User::getUser()); ?>
+							<?php foreach($groups as $gid) : ?>
+								<tr>
+									<td class="cl_group_item"><?php echo $gid; ?></td>
+								</tr>
+							<?php endforeach; ?>
 					</table>
 				</td>
 				<td>
 					<table id="table_users">
 						<tr>
-							<th>Users</th>
+							<th><?php p($l->t('Users')); ?></th>
 						</tr>
 					</table>
 				</td>
 			</tr>
 		</table>
-
-		<a id="button_close_access">Close</a>
+		<a id="button_close_access"><?php p($l->t('Close')); ?></a>
 	</div>
-
 </form>
 
